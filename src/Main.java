7fd903532d8c3316cfc1cbd7b8120b958e099759
Gokul1909 <gokul@terzocloud.com>
2023-06-14 12:53:39 +0530

    Inheritance

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,42 @@
+class Account{
+    protected long balance;
+
+    public Account(long balance){
+        this.balance=balance;
+    }
+    protected void credit(long credit)
+    {
+        balance+=credit;
+    }
+    protected void debit(long debit)
+    {
+        balance-=debit;
+    }
+}
+class SavingsAccount extends Account{  //The savings account class inherits the Account class
+    protected float interestRate;
+    public float temp_balance;
+    public SavingsAccount(long balance,float interest){
+        super(balance);
+        interestRate=interest;
+    }
+    public void showBalance()
+    {
+        System.out.println("The current balance is:"+balance);
+    }
+    public void applyInterest(int years){
+        temp_balance=balance+(balance*(interestRate/100)*years);
+        System.out.println("The balance after interest is applied for the given duration is:"+temp_balance);
+    }
+}
 public class Main { //here public is the access specifier for the class
-    //data members
-    String nameOfStudent;
-    int age;
-    long number;
-
-    //constructor for the class
-    public Main(String name,int age,long number){
-        nameOfStudent=name;
-        this.age=age; //this is used to specify that the age is the member of the class
-        this.number=number;
-    }
-
-    //member functions
-    public void getStudentDetails(){
-        System.out.println("The name of the student is:"+nameOfStudent+",age is:"+age+"and contact is:"+number);
-    }
     public static void main(String args[]){
-        Main student1=new Main("Gokul",20,9047659730L);
-        student1.getStudentDetails();
+        SavingsAccount account1=new SavingsAccount(10000L,5.13F);
+        account1.showBalance();
+        account1.credit(10000);
+        account1.showBalance();
+        account1.debit(3000);
+        account1.showBalance();
+        account1.applyInterest(3);
     }
 }
